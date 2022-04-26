b2c9acc7348a8f3f3739df2d6b9d980a641122b4
qc <chaoqini@q/qq.com>
2022-04-26 19:42:59 +0800

    20220426_19:42

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+## ==========
+set -gx EDITOR = /usr/bin/vim
+## ==========
+
+
+
+
+## >>> conda initialize >>>
+## !! Contents within this block are managed by 'conda init' !!
+##eval /opt/anaconda/bin/conda "shell.fish" "hook" $argv | source
+## <<< conda initialize <<<
+
